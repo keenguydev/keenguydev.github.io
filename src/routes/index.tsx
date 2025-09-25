80612ed17d79c33686eccf8233210027a55aa771
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-26 00:39:12 +0700

    push push

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 import { IProject } from "~/entities/project";
 import Main from "~/widgets/Main";
@@ -6,11 +6,20 @@ import Projects from "~/widgets/Projects";
 import projectsData from '../../public/json/projects.json'
 
 export default component$(() => {
-  const projects = projectsData as IProject[];
+  const projects = useResource$(async () => {
+    const response = await fetch('https://deirox.github.io/json/projects.json')
+    const data = await response.json()
+    return data
+  })
+
   return (
     <>
-      <Main  />
-      <Projects projects={projects} />
+      <Main />
+      <Resource
+        value={projects}
+        onPending={() => <>...loading</>}
+        onResolved={(_projects) => <Projects projects={_projects} />}
+      />
     </>
   );
 });
