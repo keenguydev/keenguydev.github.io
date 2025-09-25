c7bb691a5d681ea76619450849de21040e6d9b89
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-26 00:41:05 +0700

    err

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,9 @@ import { type DocumentHead } from "@builder.io/qwik-city";
 import { IProject } from "~/entities/project";
 import Main from "~/widgets/Main";
 import Projects from "~/widgets/Projects";
-import projectsData from '../../public/json/projects.json'
 
 export default component$(() => {
-  const projects = useResource$(async () => {
+  const projects = useResource$<IProject[]>(async () => {
     const response = await fetch('https://deirox.github.io/json/projects.json')
     const data = await response.json()
     return data
