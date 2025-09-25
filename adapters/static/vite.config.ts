305c24a3e21f2e1146546ff32f80229b238d9524
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-26 00:33:59 +0700

    god

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -4,6 +4,12 @@ import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
   return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ["@qwik-city-plan"],
+      },
+    },
     plugins: [
       staticAdapter({
         origin: "https://deirox.github.io",
