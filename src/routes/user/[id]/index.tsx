7d36ad28fc505ea4bd04a77a3e553f80bb392e8a
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-30 05:08:22 +0700

    ssg csr?

---
+++ src/routes/user/[id]/index.tsx
@@ -0,0 +1,13 @@
+import { component$, useResource$, Resource } from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
+
+export default component$(() => {
+    const location = useLocation();
+    const userId = location.params.id;
+    return (
+        <div>
+            <h1>Профиль пользователя</h1>
+            <p>user {userId}</p>
+        </div>
+    );
+});
