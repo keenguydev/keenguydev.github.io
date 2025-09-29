369b590662119ebda20f06304b1df0b4fbf9896a
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-30 05:24:57 +0700

    hash

--- src/routes/user/index.tsx
+++ src/routes/user/index.tsx
@@ -1,13 +1,16 @@
-import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 
 export default component$(() => {
-    const location = useLocation();
-    const userId = location.url.hash;
+    const userId = useSignal('')
+    useVisibleTask$(
+        () => {
+            userId.value = window.location.hash
+        }
+    )
     return (
         <div>
             <h1>лысый чорт</h1>
-            <p>user {userId}</p>
+            <p>user {userId.value}</p>
         </div>
     );
 });
