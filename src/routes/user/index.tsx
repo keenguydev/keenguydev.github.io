1bf7d33de04984409ab768c7041921b3100474de
keenguy <keenguyinbox+gitflic@gmail.com>
2025-09-30 05:21:30 +0700

    rip

--- src/routes/user/index.tsx
+++ src/routes/user/index.tsx
@@ -3,7 +3,7 @@ import { useLocation } from '@builder.io/qwik-city';
 
 export default component$(() => {
     const location = useLocation();
-    const userId = location.params.id;
+    const userId = location.url.hash;
     return (
         <div>
             <h1>лысый чорт</h1>
