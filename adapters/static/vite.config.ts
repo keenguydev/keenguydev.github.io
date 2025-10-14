c29c2f60cb814c13da105073367f73c05851b142
KEENGUY <41786099+keenguydev@users.noreply.github.com>
2025-10-14 18:05:48 +0700

    Update vite.config.ts

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://deirox.github.io",
+        origin: "https://keenguydev.github.io",
       }),
     ],
   };
